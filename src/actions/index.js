f706361895118c7c1b1a218f2e6328b8a414a2d5
ericmasiello <eric.j.masiello@gmail.com>
2016-01-16 19:02:01 -0500

    Add our reducer for weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,11 +9,18 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city){
 
   const url = `${ROOT_URL}&q=${city},us`;
-  //request is a promise now
+
+  //request variable is a promise
   const request = axios.get(url);
 
   return {
     type: FETCH_WEATHER,
+    /*
+     * redux-promise middleware automatically will inspect the 'payload' property
+     * to see if its a promise. if it is, it will stop the action, wait for it to resolve
+     * and then immediately dispatch the exact same action again but replaces the 'payload'
+     * to be the resolved request
+     */
     payload: request
   }
 }
